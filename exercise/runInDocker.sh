397a94a57174322ce9accd8b8145e1b4a795dc55
Haspamelodica <melodicahaspa@gmail.com>
2022-04-27 23:59:47 +0200

    Tried removing compile goal from student side

--- exercise/runInDocker.sh
+++ exercise/runInDocker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# TODO only debugging
+echo Just before Maven call
 # TODO make this less ugly and configurable
 cd tests && mvn -Dstudentcodeseparator="-t 10000 fifo out /fifos/exToStud in /fifos/studToEx" test
