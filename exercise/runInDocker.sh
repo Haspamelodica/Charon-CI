fa4f79342cb0faace2980a497e0f137a2ddc7078
Haspamelodica <melodicahaspa@gmail.com>
2022-04-28 00:01:13 +0200

    Calling export for debugging

--- exercise/runInDocker.sh
+++ exercise/runInDocker.sh
@@ -2,5 +2,6 @@
 
 # TODO only debugging
 echo Just before Maven call
+export
 # TODO make this less ugly and configurable
 cd tests && mvn -Dstudentcodeseparator="-t 10000 fifo out /fifos/exToStud in /fifos/studToEx" test
--- student/runInDocker.sh
+++ student/runInDocker.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo Just before Maven call
+export
 # TODO is the compile goal neccessary?
 mvn exec:java >/logs/out.log 2>/logs/err.log
