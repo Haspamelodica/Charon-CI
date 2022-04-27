ea452242223b9d00267e657e5092ad7a885bc66d
Haspamelodica <melodicahaspa@gmail.com>
2022-04-28 01:32:54 +0200

    More debug prints

--- student/run_in_docker.sh
+++ student/run_in_docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+# TODO only debugging
+echo Some standard output to real log
+echo Some error output to real log >&2
 exec ./run_in_docker_with_logs.sh >/logs/out.log 2>/logs/err.log
--- student/run_in_docker_with_logs.sh
+++ student/run_in_docker_with_logs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO only debugging
 echo Some standard output
 echo Some error output >&2
 exec mvn compile exec:java
