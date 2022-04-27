330a585f10400b4f692af5f2db605bdb7ddfbf85
Haspamelodica <melodicahaspa@gmail.com>
2022-04-28 01:02:47 +0200

    More debugging: explicitly echoing MAVEN_CONFIG now

--- exercise/run_in_docker.sh
+++ exercise/run_in_docker.sh
@@ -3,5 +3,6 @@
 # TODO only debugging
 echo Just before Maven call
 printenv
+echo "MAVEN_CONFIG: $MAVEN_CONFIG"
 # TODO make this less ugly and configurable
 cd tests && exec mvn -Dstudentcodeseparator="-t 10000 fifo out /fifos/exToStud in /fifos/studToEx" test
--- student/run_in_docker_with_logs.sh
+++ student/run_in_docker_with_logs.sh
@@ -3,4 +3,5 @@
 # TODO only debugging
 echo Just before Maven call
 printenv
+echo "MAVEN_CONFIG: $MAVEN_CONFIG"
 exec mvn compile exec:java
