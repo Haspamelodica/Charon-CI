7185211d3685fed992350aaf4df72ffb59d2634b
Haspamelodica <melodicahaspa@gmail.com>
2022-04-28 02:50:36 +0200

    chown_to_current_user now uses base instead of exercise
    Also removed one artifact again.

--- chown_to_current_user.sh
+++ chown_to_current_user.sh
@@ -3,4 +3,4 @@
 export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
-exec docker run -u 0 -v $(readlink -f $1):/data studentcodeseparator:exercise chown -R $HOST_UID:$HOST_GID /data
+exec docker run -u 0 -v $(readlink -f $1):/data studentcodeseparator:base chown -R $HOST_UID:$HOST_GID /data
