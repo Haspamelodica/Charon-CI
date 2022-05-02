e33a923005642c43a759a7a82cf76833ed34f5b2
Haspamelodica <melodicahaspa@gmail.com>
2022-05-03 01:30:12 +0200

    Added base to pull_containers becaues of chown_to_current_user

--- pull_containers.sh
+++ pull_containers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker pull ghcr.io/haspamelodica/studentcodeseparator:base
 docker pull ghcr.io/haspamelodica/studentcodeseparator:exercise
 docker pull ghcr.io/haspamelodica/studentcodeseparator:student-base
