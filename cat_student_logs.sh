996467f8e9c1e17356ddba5a2881aa9dbde825de
Haspamelodica <melodicahaspa@gmail.com>
2022-06-16 14:15:32 +0200

    Student container name isn't hardcoded anymore

--- cat_student_logs.sh
+++ cat_student_logs.sh
@@ -4,8 +4,7 @@ echo "---Student side log start---"
 # Make sure stdout has been flushed. Avoids reordering stderr output before the start message.
 sleep 1
 
-docker logs charon-student
-
+docker logs "$1"
 # Make sure stderr has been flushed. Avoids reordering stderr output after the end message.
 sleep 1
 echo "---Student side log end---"
--- run_exercise.sh
+++ run_exercise.sh
@@ -4,17 +4,16 @@
 ./setup_exercise.sh || exit $?
 
 # Run student container
-docker run \
+student_container_name=$(docker run \
 		--volume $(readlink -f fifos):/fifos \
 		--detach \
-		--name charon-student \
-		charon:student \
+		charon:student) \
 || {
 	student_exit_code=$?
 	echo "Starting student container failed with $student_exit_code"
 
 	# Still try to stop it in case it was started even if docker run reported an error. Probably fails, so ignore errors there.
-	docker rm -f charon-student || (echo "Removing student container failed with $?; it probably wasn't started at all." >&2)
+	docker rm -f "$student_container_name" || (echo "Removing student container failed with $?; it probably wasn't started at all." >&2)
 
 	# Don't try to continue
 	exit $student_exit_code
@@ -45,23 +44,25 @@ docker run \
 # Also, ignore any errors and pipe stderr to /dev/null:
 # docker stop fails if the container shut down by itself,
 # and we remove the container later on either way.
-docker stop charon-student 2>/dev/null
+docker stop "$student_container_name" 2>/dev/null
 
-# Cat student log files so they appear in regular log.
-./cat_student_logs.sh \
+# Cat student logs so they appear in regular log.
+./cat_student_logs.sh "$student_container_name" \
 || {
 	# Don't overwrite previous error codes
 	exit_code_cat=$?
 	if [ "$exit_code" == "0" ]; then exit_code=$exit_code_cat; fi
-	echo "Cat student log files failed with $exit_code_cat" >&2
+	echo "Cat student logs failed with $exit_code_cat" >&2
 }
 
 # Remove student container
-docker rm -f charon-student \
+docker rm -f "$student_container_name" \
 || {
 	# Overwrite previous error codes
 	exit_code=$?
 	echo "Removing student container failed with $exit_code. This means it might still be running!" >&2
+	# Echo student container name in case somebody wants to cleanup "by hand"
+	echo "Student container name: $student_container_name" >&2
 }
 
 exit $exit_code
