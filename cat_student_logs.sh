4b33b13b0b59fcaf3d9448715a47a65aaf8eb56a
Haspamelodica <melodicahaspa@gmail.com>
2023-03-22 19:43:47 +0100

    cat_student_logs.sh no longer blocks, also log is easier to read
    when viewing stdout and stderr separately.

--- cat_student_logs.sh
+++ cat_student_logs.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo "---Student side log start---"
-# Make sure stdout has been flushed. Avoids reordering stderr output before the start message.
-sleep 1
+echo "---Student side log start---" >&2
 
 docker logs "$1"
-# Make sure stderr has been flushed. Avoids reordering stderr output after the end message.
-sleep 1
+
 echo "---Student side log end---"
+echo "---Student side log end---" >&2
