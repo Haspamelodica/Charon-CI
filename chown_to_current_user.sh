3921d2ec781b53d439861123f508ba919ad45d8f
Haspamelodica <melodicahaspa@gmail.com>
2023-04-20 16:33:40 +0200

    chown_to_current_user no longer leaves containers alive

--- chown_to_current_user.sh
+++ chown_to_current_user.sh
@@ -4,4 +4,9 @@ export HOST_UID=$(id -u)
 export HOST_GID=$(id -g)
 
 # TODO find better way to execute as root; or at least use scratch image.
-exec docker run -u 0 -v $(readlink -f $1):/data ghcr.io/haspamelodica/charon:exercise chown -R $HOST_UID:$HOST_GID /data
+exec docker run \
+		--user 0 \
+		--volume $(readlink -f $1):/data \
+		--rm \
+		ghcr.io/haspamelodica/charon:exercise \
+		chown -R $HOST_UID:$HOST_GID /data
