7821cd752c452bab5c06df350c4be06a9d2393bd
Haspamelodica <melodicahaspa@gmail.com>
2023-04-24 20:13:33 +0200

    Removed connection timeout from exercise

--- exercise/run_in_docker.sh
+++ exercise/run_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd tests && exec mvn -Dnet.haspamelodica.charon.communicationargs="-t 10000 fifo out /fifos/exToStud in /fifos/studToEx" test
+cd tests && exec mvn -Dnet.haspamelodica.charon.communicationargs="fifo out /fifos/exToStud in /fifos/studToEx" test
