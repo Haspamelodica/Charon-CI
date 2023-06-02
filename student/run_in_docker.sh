275535d4ddc333f9fa4c8899c559fbc1bbbbcc14
Haspamelodica <melodicahaspa@gmail.com>
2023-06-02 17:47:06 +0200

    Student side runner reports compilation errors

--- student/run_in_docker.sh
+++ student/run_in_docker.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-exec mvn compile exec:java
+if mvn compile; then
+	# No timeout: The test may take a long time to initialize.
+	# Also, the environment must be able to kill the student container
+	# even if the test side never connects and the student container freezes,
+	# because a student could mimick this maliciously.
+	exec mvn -Dnet.haspamelodica.charon.communicationargs="fifo in /fifos/exToStud out /fifos/studToEx" exec:java
+else
+	# Open the reading end so the exercise side succeeds in opening both FIFOs
+	cat /fifos/exToStud > /dev/null &
+	# Echo the marker for "compilation error"
+	echo -n "c" > /fifos/studToEx
+fi
