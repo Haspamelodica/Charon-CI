9639efb2ecba86bf663e9a073d8a671037dfb81e
Haspamelodica <melodicahaspa@gmail.com>
2023-06-02 17:29:51 +0200

    Student image is no longer tagged with some fixed tag

--- student/build_container_echo_id.sh
+++ student/build_container_echo_id.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#TODO don't tag with fixed name charon:student! Instead, somehow transmit the ID.
-exec docker build -q -t charon:student -f student.Dockerfile .
+exec docker build -q -f student.Dockerfile .
